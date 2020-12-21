3b825532e5fc122dd720ffd74184336b2e09eff6
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2020-12-21 17:49:31 +0100

    Add TextScreen component

---
+++ src/screens/TextScreen.js
@@ -0,0 +1,17 @@
+import React from 'react';
+import { Text, StyleSheet, View, TextInput, ViewBase } from 'react-native';
+const TextScreen = () => {
+  return (
+    <View>
+      <TextInput style={styles.input} autoCapitalize='none' autoCorrect={false} />
+    </View>
+  );
+};
+const styles = StyleSheet.create({
+  input: {
+    margin: 15,
+    borderColor: 'black',
+    borderWidth: 1,
+  },
+});
+export default TextScreen;
