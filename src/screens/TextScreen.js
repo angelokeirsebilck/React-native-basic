e8233cd17ded262a396628f57a694e7dbd9cd6f2
angelokeirsebilck <angelo.keirsebilck1@telenet.be>
2020-12-21 17:54:22 +0100

    Add onChangeText

--- src/screens/TextScreen.js
+++ src/screens/TextScreen.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Text, StyleSheet, View, TextInput, ViewBase } from 'react-native';
 const TextScreen = () => {
+  const [name, setName] = useState('');
+
   return (
     <View>
-      <TextInput style={styles.input} autoCapitalize='none' autoCorrect={false} />
+      <TextInput
+        style={styles.input}
+        autoCapitalize='none'
+        autoCorrect={false}
+        value={name}
+        onChangeText={(newValue) => setName(newValue)}
+      />
+      <Text>My name is {name}</Text>
     </View>
   );
 };
